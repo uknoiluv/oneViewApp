48bc2babb7287ae3ce62b1783aad935582ca4662
Adrian Kim <uknoiluv@gmail.com>
2014-10-28 20:05:44 -0700

    10/28_searchBox_filter

--- server.js
+++ server.js
@@ -3,14 +3,10 @@ var path = require('path');
 
 var express = require('express');
 var app = express();
-// var bodyParser = require('body-parser');
 
 var dataArr = require('./data.js');
 
-// app.use(express.static(__dirname + './public'));
 app.use(express.static(__dirname));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
   res.render('index');
@@ -22,6 +18,4 @@ var server = app.listen(3000, function(){
 
 app.get('/data', function(req, res){
   res.json(dataArr.data);
-  // console.log('res', res);
-  // res.send('hello world!!');
 });
